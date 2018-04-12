8d3fd405b111c2303a3a4bec11f4d47f77400b82
Ka-Ping Yee <zestyping@gmail.com>
2018-04-11 22:01:30 -0700

    Add a plain MapsForge map to the main activity.

--- app/src/main/java/ca/zesty/fleetreceiver/MainActivity.java
+++ app/src/main/java/ca/zesty/fleetreceiver/MainActivity.java
@@ -7,10 +7,29 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
+import org.mapsforge.map.android.util.AndroidUtil;
+import org.mapsforge.map.android.view.MapView;
+import org.mapsforge.map.datastore.MapDataStore;
+import org.mapsforge.map.datastore.MultiMapDataStore;
+import org.mapsforge.map.layer.cache.TileCache;
+import org.mapsforge.map.layer.renderer.TileRendererLayer;
+import org.mapsforge.map.reader.MapFile;
+import org.mapsforge.map.rendertheme.InternalRenderTheme;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 
 public class MainActivity extends BaseActivity {
     static final String TAG = "MainActivity";
@@ -19,15 +38,16 @@ public class MainActivity extends BaseActivity {
 
     private LogMessageReceiver mLogMessageReceiver = new LogMessageReceiver();
     private AppDatabase mDb = AppDatabase.getDatabase(this);
+    private MapView mapView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Fleet Receiver " + BuildConfig.VERSION_NAME);
 
         ActivityCompat.requestPermissions(this, new String[] {
             Manifest.permission.INTERNET,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.READ_PHONE_STATE,
             Manifest.permission.RECEIVE_BOOT_COMPLETED,
             Manifest.permission.RECEIVE_SMS,
@@ -35,8 +55,17 @@ public class MainActivity extends BaseActivity {
             Manifest.permission.WAKE_LOCK
         }, 0);
 
+        AndroidGraphicFactory.createInstance(getApplication());
+        initializeMap((MapView) findViewById(R.id.map));
+
         startService(new Intent(getApplicationContext(), ReceiverService.class));
         registerReceiver(mLogMessageReceiver, new IntentFilter(ACTION_FLEET_RECEIVER_LOG_MESSAGE));
+    }
+
+    @Override protected void onDestroy() {
+        mapView.destroyAll();
+        AndroidGraphicFactory.clearResourceMemoryCache();
+        super.onDestroy();
     }
 
     @Override public void onRequestPermissionsResult(
@@ -62,6 +91,69 @@ public class MainActivity extends BaseActivity {
             startActivity(new Intent(this, RegistrationActivity.class));
         }
         return false;
+    }
+
+    void initializeMap(MapView mapView) {
+        TileCache tileCache = AndroidUtil.createTileCache(this, "mapcache",
+            mapView.getModel().displayModel.getTileSize(), 1f,
+            mapView.getModel().frameBufferModel.getOverdrawFactor());
+        MultiMapDataStore multiMap = new MultiMapDataStore(
+            MultiMapDataStore.DataPolicy.RETURN_ALL);
+        addMapFile(multiMap, getAssetFile("world.map"));
+        addMapFilesInDir(multiMap, Environment.getExternalStoragePublicDirectory(
+            Environment.DIRECTORY_DOWNLOADS));
+        TileRendererLayer tileRendererLayer = new TileRendererLayer(tileCache, multiMap,
+            mapView.getModel().mapViewPosition, AndroidGraphicFactory.INSTANCE);
+        tileRendererLayer.setXmlRenderTheme(InternalRenderTheme.DEFAULT);
+
+        mapView.getLayerManager().getLayers().add(tileRendererLayer);
+        mapView.setCenter(multiMap.startPosition());
+        mapView.setZoomLevel(multiMap.startZoomLevel());
+        mapView.setClickable(true);
+        mapView.getMapScaleBar().setVisible(true);
+        mapView.setBuiltInZoomControls(true);
+    }
+
+    void addMapFilesInDir(MultiMapDataStore multiMap, File dir) {
+        for (File file : dir.listFiles()) {
+            if (file.isDirectory()) {
+                addMapFilesInDir(multiMap, file);
+            }
+            if (file.isFile() && file.getName().toLowerCase().endsWith(".map")) {
+                addMapFile(multiMap, file);
+            }
+        }
+    }
+
+    void addMapFile(MultiMapDataStore multiMap, File file) {
+        try {
+            MapDataStore map = new MapFile(file);
+            multiMap.addMapDataStore(map, true, true);
+            Log.i(TAG, "Loaded map from " + file.getAbsolutePath());
+        } catch (Exception e) {
+            Log.e(TAG, "Could not load map from " + file.getAbsolutePath(), e);
+            e.printStackTrace();
+        }
+    }
+
+    File getAssetFile(String name) {
+        File destination = new File(getCacheDir(), name);
+        if (!destination.exists()) {
+            try {
+                InputStream in = getAssets().open(name);
+                OutputStream out = new FileOutputStream(destination);
+                byte[] buffer = new byte[8192];
+                int count = in.read(buffer);
+                while (count >= 0) {
+                    out.write(buffer, 0, count);
+                    count = in.read(buffer);
+                }
+                out.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Could not copy asset to " + destination, e);
+            }
+        }
+        return destination;
     }
 
     class LogMessageReceiver extends BroadcastReceiver {
